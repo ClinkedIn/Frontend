c255789e40c9d39d34a7860604696d53e6c4fcb9
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2025-03-13 02:23:07 +0200

    addition: mocks, utils and constants for url

---
+++ src/utils/axiosInterceptor.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { baseUrl } from '../constants';
+import { getRequest } from '../services/axios';
+
+const axiosInterceptor = axios.interceptors.response.use(async (response) => {
+    return response;
+}, async (error) => {
+    if (error.response) {
+
+        const originalRequest = error.config;
+        if (error.response.status === 403 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshTokenResponse = await getRequest(`${baseUrl}/user/refresh-token`);
+            if (refreshTokenResponse && refreshTokenResponse.status === 200) {
+                const res = await axios(originalRequest);
+                return res;
+            } else {
+                return Promise.reject(refreshTokenResponse ? refreshTokenResponse.data : 'Unknown error')
+            }
+        }
+    } else {
+        console.log('Error', error.message);
+    }
+
+    return Promise.reject(error);
+});
+
+export default axiosInterceptor;
