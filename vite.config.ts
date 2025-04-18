ecf1dbfd7613907de6eea8ff83fd2dd95e3c7f26
tahaaa22 <ahmed.taha.oman@gmail.com>
2025-04-18 13:54:10 +0200

    Add vite-plugin-compression for gzip support in build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import viteCompression from 'vite-plugin-compression';
 
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    test: {
-    globals: true, 
-    environment: 'jsdom',
-     setupFiles: ['./setupTests.js'],
+  plugins: [react(), tailwindcss(),
+  viteCompression({
+    algorithm: 'gzip',
+    ext: '.gz',
+    threshold: 10240,
+  }),
+  ],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+    },
+  }
   },
 });
