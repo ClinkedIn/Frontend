e0a8c6ef0425431f4225b8d7e46e0e195bb67233
seiifhesham <115505623+seiifhesham@users.noreply.github.com>
2025-05-02 20:17:19 +0300

    feat: network page

--- src/components/authentication/Login/LoginPage.tsx
+++ src/components/authentication/Login/LoginPage.tsx
@@ -119,7 +119,7 @@ const LoginPage = () => {
   >({
     mutationFn: async ({ email, password }) =>
       await axios.post(
-        `${BASE_URL}/api/user/login`,
+        `${BASE_URL}/user/login`,
         { email, password },
         { withCredentials: true }
       ),
